c42b12656a326b5c972297a947d7b472c754fc30
paunstefan <paunstefan@protonmail.com>
2023-04-12 19:35:41 +0300

    Camera info. Error handling.

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AstroPhiError {
+    #[error("GPhoto2 error")]
+    GPhoto2(#[from] gphoto2::Error),
+    #[error("ParseInt error")]
+    ParseInt(#[from] std::num::ParseIntError),
+    #[error("ParseFloat error")]
+    ParseFloat(#[from] std::num::ParseFloatError),
+}
+
+impl IntoResponse for AstroPhiError {
+    fn into_response(self) -> Response {
+        let error_text = match self {
+            AstroPhiError::GPhoto2(error) => format!("{:?}", error.kind()),
+            AstroPhiError::ParseInt(error) => error.to_string(),
+            AstroPhiError::ParseFloat(error) => error.to_string(),
+        };
+        (StatusCode::INTERNAL_SERVER_ERROR, error_text).into_response()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, response::Html, routing::get, Json, Router};
+use error::AstroPhiError;
+use gphoto2::{widget::RadioWidget, Context};
+use serde::{Deserialize, Serialize};
+use std::{fs, net::SocketAddr};
+
+mod error;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/info", get(camera_info));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// Handler for the main page
+async fn root() -> Result<Html<String>, StatusCode> {
+    let html = Html(fs::read_to_string("static/index.html").map_err(|_| StatusCode::NOT_FOUND)?);
+    Ok(html)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CameraInfo {
+    pub iso: u32,
+    pub aperture: f32,
+    pub exposure: f32,
+    pub capturetarget: String,
+}
+
+async fn camera_info() -> Result<Json<CameraInfo>, AstroPhiError> {
+    let camera = Context::new()?.autodetect_camera().wait()?;
+
+    let iso: u32 = camera
+        .config_key::<RadioWidget>("iso")
+        .wait()?
+        .choice()
+        .parse()?;
+    tracing::debug!("Parsed ISO: {:?}", iso);
+
+    let exposure: f32 = camera
+        .config_key::<RadioWidget>("shutterspeed")
+        .wait()?
+        .choice()
+        .parse()?;
+    tracing::debug!("Parsed shutterspeed: {:?}", exposure);
+
+    let aperture: f32 = camera
+        .config_key::<RadioWidget>("aperture")
+        .wait()?
+        .choice()
+        .parse()?;
+    tracing::debug!("Parsed aperture: {:?}", aperture);
+
+    // "Internal RAM" = 0
+    // "Memory card"  = 1
+    let capturetarget = camera
+        .config_key::<RadioWidget>("capturetarget")
+        .wait()?
+        .choice();
+    tracing::debug!("Parsed capturetarget: {:?}", capturetarget);
+
+    let info = CameraInfo {
+        iso,
+        aperture,
+        exposure,
+        capturetarget,
+    };
+
+    Ok(Json(info))
 }
